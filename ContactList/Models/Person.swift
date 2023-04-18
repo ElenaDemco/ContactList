004578714919fa9ecd5d66be6d55545ce14c5d04
Elena <elena.demco@gmail.com>
2023-04-18 22:11:15 +0200

    Person struct create

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -7,4 +7,27 @@
 
 struct Person {
     
+    let name: String
+    let surname: String
+    
+    let phone: String
+    let email: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
 }
+
+extension Person {
+    static func getContactList() -> [Person] {
+        let person: [Person] = []
+        
+        let names = DataStore.info.names.shuffled()
+        let surnames = DataStore.info.surnames.shuffled()
+        let phones = DataStore.info.phones.shuffled()
+        let emails = DataStore.info.emails.shuffled()
+        return person
+    }
+}
+
+

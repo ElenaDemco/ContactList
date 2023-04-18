0ca965e38d0f5afd62edd127ceea6bbd3cba678f
Elena <elena.demco@gmail.com>
2023-04-18 22:11:55 +0200

    Details page create

--- ContactList/ViewControllers/ContactDetailsViewController.swift
+++ ContactList/ViewControllers/ContactDetailsViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 final class ContactDetailsViewController: UIViewController {
     
+    
+    @IBOutlet var phonePerson: UILabel!
+    @IBOutlet var emailPerson: UILabel!
+    
+    var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = person.fullName
+        
+        phonePerson.text = "Phone: \(person.phone)"
+        emailPerson.text = "E-mail: \(person.email)"
     }
 }

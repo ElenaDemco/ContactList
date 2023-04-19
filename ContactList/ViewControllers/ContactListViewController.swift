b0a4094bbf05fdb4108c2c4de9ed1482d07def65
Elena <elena.demco@gmail.com>
2023-04-19 22:10:15 +0200

    Added extension and some amendments to the code

--- ContactList/ViewControllers/ContactListViewController.swift
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 final class ContactListViewController: UITableViewController {
     
-    private let contactList = Person.getContactList()
+    var contactList = Person.getContactList()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.rowHeight = 60
-    }
-    
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
         let person = contactList[indexPath.row]
-        let detailsVC = segue.destination as? ContactDetailsViewController
-        detailsVC?.person = person
+        
+        if segue.identifier == "showDetails" {
+            let detailsVC = segue.destination as? ContactDetailsViewController
+            detailsVC?.person = person
+        }
     }
 }
 
+// MARK: - UITableViewDataStore
 extension ContactListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         contactList.count
@@ -31,12 +31,22 @@ extension ContactListViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
+        
         var content = cell.defaultContentConfiguration()
         let contact = contactList[indexPath.row]
-        content.text = contact.name
-        content.secondaryText = contact.fullName
+        
+        content.text = contact.fullName
         cell.contentConfiguration = content
         
         return cell
     }
 }
+
+// MARK: - UITableViewDelegate
+extension ContactListViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let contact = contactList[indexPath.row]
+        performSegue(withIdentifier: "showDetails", sender: contact)
+    }
+}
+
